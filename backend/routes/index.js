133671322599f71ddf5ea9486c33e77c145e3d6e
Balogh Csaba <35346323+Csabesz1130@users.noreply.github.com>
2024-06-08 20:28:43 +0200

    Update index.js to include authRoutes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./userRoutes'));
+router.use('/auth', require('./authRoutes'));
+
 
 module.exports = router;
